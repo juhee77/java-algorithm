60d23b24f0041d39fd273d8cd1b8e79f212977db
juhee77 <bora2233@naver.com>
2022-02-05 02:59:07 +0900

    run time error

--- src/Binary0205.java
+++ src/Binary0205.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Binary0205 {
+import java.util.*;
+
+class Binary0205  {
+    public static int solution(int n) {
+        int answer = 0;
+        String dec = "";
+        while(3<=n){
+            String temp = Integer.toString(n%3);
+            n = n/3;
+            dec = dec.concat(temp);
+        }
+        String temp = Integer.toString(n%3);
+        dec = dec.concat(temp);
+        int decN = Integer.parseInt(dec);
+
+        int i=1;
+        while(10<=decN){
+            answer += (decN % 10)*i;
+            decN = decN / 10;
+            i*=3;
+        }
+        answer += decN*i;
+        return answer;
+    }
+
+    public static void main(String args[]){
+        int numbers= 45;
+        System.out.println(solution(numbers));
+        int numbers1= 125;
+        System.out.println(solution(numbers1));
+    }
 }
