d3e046040a9bcd55b4beb4ed8620b63db652ebe4
juhee77 <bora2233@naver.com>
2022-02-09 20:32:03 +0900

    0209

--- src/프로그래머스/체육복.java
+++ src/프로그래머스/체육복.java
@@ -1,16 +1,30 @@
 package 프로그래머스;
 import java.util.*;
+//https://programmers.co.kr/learn/courses/30/lessons/42862/solution_groups?language=java
 public class 체육복 {
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = n-lost.length;
         int index=0;
 
+        for(int i=0;i<lost.length;i++){
+            for (int j=0;j<reserve.length;j++){
+                if(lost[i]==reserve[j]){
+                    answer++;
+                    reserve[j]=-1;
+                    lost[i]=-1;
+                    break;
+                }
+            }
+        }
+
         Arrays.sort(lost);
         Arrays.sort(reserve);
+        //복잡도 부분에서 안좋을듯
+        //그냥 사람명수만큼 배열을 만들어서 lost-1, reserve +1로 해서 사람끼리 비교하는게 복작도가 더 좋을것 같다
 
         for(int i=0;i<lost.length;i++){
             for(int j=index;j<reserve.length;j++){
-                if(lost[i]-1 ==reserve[j]||lost[i]==reserve[j]||lost[i]+1 == reserve[j] ) {
+                if(lost[i]-1 ==reserve[j]||lost[i]+1 == reserve[j] ) {
                     answer++;
                     index=j+1;
                     reserve[j]=-1;
