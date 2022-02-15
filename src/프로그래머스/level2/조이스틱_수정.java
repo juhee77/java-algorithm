2264f232b41af5e93802f2e5c3aee994f2239c1a
lahezy <jouhi3322@gmail.com>
2022-02-15 21:28:09 +0900

    0215_have to modify

--- src/프로그래머스/level2/조이스틱_수정.java
+++ src/프로그래머스/level2/조이스틱_수정.java
@@ -11,22 +11,24 @@ public class 조이스틱_수정 {
         /*문자에 대한*/
         for(int i=0;i<charName.length;i++){
             int charId=Character.getNumericValue(charName[i]);
-            System.out.print(charName[i]+" "+charId+"   ");
+            //System.out.print(charName[i]+" "+charId+"   ");
             //A부터 시작하는경우
             int greed_A=charId-10;
             int greed_Z=35-charId+1;
             //if(i>0 && Character.getNumericValue(charName[i-1])==10){
             //    greed_Z=35-charId;
             //}
-            System.out.println(Math.min(greed_A,greed_Z));
+            //System.out.println(Math.min(greed_A,greed_Z));
             answer = answer + Math.min(greed_A,greed_Z);
 
             if(charId!=10)
                 whe.add(i);
 
         }
+        //System.out.println("answer "+answer+" length "+charName.length+" sieze "+whe.size());
 
         /*위치에 대한*/
+        /*
         int ret=0;
         for(int i=0;i< whe.size();i++){
             int forward=whe.get(i)-ret;
@@ -34,8 +36,25 @@ public class 조이스틱_수정 {
             answer+=Math.min(forward,backward);
             ret=whe.get(i);
         }
+        */
 
-        System.out.println();
+        if(whe.size()!= charName.length) {
+            int maxStart = -1;
+            int maxEnd = charName.length + 1;
+            int max = 0;
+            for (int i = 0; i < whe.size() - 1; i++) {
+                if (whe.get(i + 1) - whe.get(i) > max && whe.get(i + 1) - whe.get(i) > 1) {
+                    max = whe.get(i + 1) - whe.get(i);
+                    maxStart = whe.get(i);
+                    maxEnd = whe.get(i + 1);
+                    //System.out.println(maxStart+":"+maxEnd);
+                }
+            }
+            answer = answer + Math.min(charName.length - 1, 2 * maxStart + 1 + charName.length-1 - maxEnd);
+        }
+        else
+            answer = answer + charName.length - 1;
+        //System.out.println();
         return answer;
     }
     public static void main(String args[]){
