cd77b9bdd6c7dec710c4a69dbf3b11fb4b172a01
juhee77 <bora2233@naver.com>
2022-02-17 01:51:13 +0900

    1193

---
+++ src/test.java
@@ -0,0 +1,18 @@
+public class test {
+    public static void main(String args[]){
+        int x=5,y=0,z=0;
+        y=x++;
+        z=--x;
+        System.out.println(x+","+y+","+z);
+        int i=0;
+        for(;i<10;i++){
+
+        }
+        int j=10;
+        for(;0<j;j--){
+
+        }
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
---
+++ src/백준/분수찾기.java
@@ -0,0 +1,24 @@
+package 백준;
+import java.util.Scanner;
+
+public class 분수찾기 {
+    public static String solution(int n){
+        int i=1;
+        for(;i<n;i++){
+            if(n>i)
+                n = n-i;
+            else
+                break;
+        }
+
+        if(i%2==0)
+            return n+"/"+(i-n+1);
+        else
+            return (i-n+1)+"/"+n;
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner((System.in));
+        System.out.println(solution(sc.nextInt()));
+    }
+}
