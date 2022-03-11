ecea7902b89dfbe255cdd8a1fac71dc2ebbb9055
juhee77 <bora2233@naver.com>
2022-03-12 00:05:23 +0900

    0312

--- src/프로그래머스/level1/keyPad.java
+++ src/프로그래머스/level1/keyPad.java
@@ -4,44 +4,50 @@ public class keyPad {
 
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        int left=0,right=0;
+        int left=10,right=12;
         for(int ele : numbers){
             System.out.println("left"+left+" right"+right+" ele"+ele);
-            if(ele %3 ==1) {
+            System.out.println("left"+left+" right"+right+" ele"+ele);
+            int temp=(ele==0)? 11: ele;
+
+            if(temp %3 ==1) {
                 answer+="L";
-                left=ele;
+                left=temp;
             }
-            else if(ele%3==0) {
+            else if(temp%3==0) {
                 answer+="R";
-                right=ele;
+                right=temp;
             }
             else{
                 int rightD=0,leftD=0;
-                if(left%3==1 )leftD=Math.abs(left/3-ele/3)+1;
-                else leftD=Math.abs(ele/3-left/3);
 
-                if(right%3==0&& ele !=0)rightD=Math.abs(ele/3-right/3)+1+1;
-                else rightD=Math.abs(ele/3-right/3);
+                if(left%3==1 )leftD=Math.abs(left/3-temp/3)+1;
+                else leftD=Math.abs(temp/3-left/3);
+
+                // (/3을 아래처럼 나누지 않고 -1을 하여 갈라지게 하면 더 간단해짐
+                if(right%3==0 && right<temp)rightD=temp/3-right/3+1+1;
+                else if(right%3==0&& right>=temp) rightD=right/3-temp/3;
+                else rightD=Math.abs(temp/3-right/3);
 
                 System.out.println(leftD + " " +rightD);
 
                 if(leftD==rightD){
                     if(hand.equals("right")){
                         answer+="R";
-                        right=ele;
+                        right=temp;
                     }
                     else{
                         answer+="L";
-                        left=ele;
+                        left=temp;
                     }
                 }
                 else if(leftD<rightD){
                     answer+="L";
-                    left=ele;
+                    left=temp;
                 }
                 else{
                     answer+="R";
-                    right=ele;
+                    right=temp;
                 }
             }
         }
