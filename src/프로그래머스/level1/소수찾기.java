cd9c3311546595a5ef76afbe9376cb0678c7bcf6
juhee77 <bora2233@naver.com>
2022-03-27 16:13:12 +0900

    다음 큰 숫자 <0326,0327>

--- src/프로그래머스/level1/소수찾기.java
+++ src/프로그래머스/level1/소수찾기.java
@@ -16,6 +16,22 @@ public class 소수찾기 {
 
         return cnt;
     }
+
+    public int solution1(int n){ //런타임 에러 나는경우 sqrt +1 or 자료형 바꾸기!!!!
+        int prime[] = new int[n+1];
+        prime[0]=prime[1]=1;
+        int cnt=0;//2
+
+        for(int i=2;i<Math.sqrt(n)+1;i++){ //제곱근 만큼 돌아도 계산 가능
+            if(prime[i]==1) continue;
+            for(int j=i*i;j<n+1;j+=i) prime[j]=1; //에라토스 테네스의 체 사용
+        }
+
+        for(int i=1;i<n+1;i++)
+            if(prime[i]==0)cnt++;
+
+        return cnt;
+    }
     public static int solution2(int n) {
         boolean prime[] = new boolean[n+1];
         prime[0]=prime[1]=true;
--- src/프로그래머스/level2/nextMaxNum.java
+++ src/프로그래머스/level2/nextMaxNum.java
@@ -1,8 +1,52 @@
 package 프로그래머스.level2;
+//https://programmers.co.kr/learn/courses/30/lessons/12911 -- 다음 큰 숫자 <0326,0327>
+import java.math.BigInteger;
 
 public class nextMaxNum {
-
     public int solution(int n) {
+        int onecnt =  countOne(Integer.toBinaryString(n));
+        String reAns = Integer.toBinaryString(n+1);
+        while(true){
+            if(onecnt == countOne(reAns)) break;
+            reAns = Integer.toBinaryString(Integer.parseInt(reAns,2)+1);
+        }
+        return Integer.parseInt(reAns,2);
+    }
+    public int countOne(String n){
+        int cnt=0;
+        for(int i=0;i<n.length();i++){
+            if(n.charAt(i)=='1') cnt++;
+        }
+        return cnt;
+    }
+
+    public int solution5(int n) {
+        char[] arr = Integer.toBinaryString(n).toCharArray();
+        int onecnt=0;
+        int fullone=0;
+        String b2="";
+        boolean flag=false;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]=='1'){
+                onecnt++;
+                fullone++;
+            }
+            if (!flag) {
+                if (arr[i] == '0' && arr[i + 1] == '1') {
+                    b2 = b2 + '1';
+                    flag = true;
+                    onecnt--;
+                }
+                else if (arr[i] == '1' ) {b2 += '1'; onecnt --;}
+                else b2 += '0';
+            }
+        }
+        if(fullone == arr.length) return Integer.parseInt("10"+"1".repeat(fullone-1),2);
+        if(flag) return Integer.parseInt(b2+ "0".repeat(arr.length-b2.length()-onecnt)+"1".repeat(onecnt),2);
+        return Integer.parseInt(b2,2);
+    }
+
+    public int solution3(int n) {
         String b1 = Integer.toBinaryString(n);
         String b2 = ""+b1.charAt(0);
         int oneCnt =0;
@@ -10,7 +54,7 @@ public class nextMaxNum {
         if(oneCnt == b1.length()) return Integer.parseInt(b2 = "10"+"1".repeat(oneCnt-1),2);
 
         oneCnt--; // 한자리 이동후 부터 생각
-        for(int i=1;i<b1.length();i++){
+        for(int i=1;i<b1.length()-1;i++){
             if(b1.charAt(i)=='0'&& b1.charAt(i+1)=='1') {
                 b2= b2+ '1' + "0".repeat(b1.length()-b2.length()-oneCnt)+"1".repeat(oneCnt-1);
                 break;
@@ -44,7 +88,7 @@ public class nextMaxNum {
         return answer;
     }
     public static void main(String args[]){
-        System.out.println(new nextMaxNum().solution(15));
+        System.out.println(new nextMaxNum().solution(78));
     }
 
 }
