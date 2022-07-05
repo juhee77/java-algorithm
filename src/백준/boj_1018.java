6d672edb6f4fcf9738029f9f1e770de97b87a2ee
juhee park <jouhi3322@gmail.com>
2022-07-06 01:13:06 +0900

    0706_boj

---
+++ src/백준/boj_1018.java
@@ -0,0 +1,54 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1018 {
+    public static int check(char arr[][], int x, int y){
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=x;i<x+8;i++){
+            for(int j=y;j<y+8;j++) {
+                char temp = (i+j)%2==0? 'W':'B';
+                if(arr[i][j]==temp) continue;
+                else cnt1++;
+            }
+            for(int j=y;j<y+8;j++) {
+                char temp = (i+j)%2==1? 'W':'B';
+                if(arr[i][j]==temp) continue;
+                else cnt2++;
+            }
+        }
+        return (cnt1>cnt2)?cnt2:cnt1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int row= Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+        char arr[][] = new char[row][col];
+        //int cnt[][] = new int [row-8][col-8];
+        int min=1000;
+
+        for(int i=0;i<row;i++){
+            String temp = br.readLine();
+            arr[i]=temp.toCharArray();
+        }
+
+        for(int i=0;i<=row-8;i++){
+            for(int j=0;j<=col-8;j++){
+                int temp = check(arr,i,j);
+                min= (temp<min)?temp:min;
+                if(min ==0) break;
+            }
+            if(min ==0) break;
+        }
+        System.out.println(min);
+    }
+}
---
+++ src/백준/boj_1436.java
@@ -0,0 +1,28 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int start =665;
+
+        int size = Integer.parseInt(br.readLine());
+
+        while(size>0){ //point 그냥 모두 돌아가버리게
+            start++;
+            String temp = Integer.toString(start);
+            if(temp.contains("666")){ //point
+                size--;
+                //System.out.println(start);
+            }
+
+        }
+        System.out.println(start);
+
+    }
+}
