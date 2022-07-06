bd32ed76a34fb58ff7f7ac7c1235352b12e56aaf
juhee park <jouhi3322@gmail.com>
2022-07-06 15:17:10 +0900

    Fibonacci

---
+++ src/백준/boj_10870.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_10870 {
+    public static int fibonacci(int n){
+        if(n==0) return 0;
+        else if(n==1)return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+}
