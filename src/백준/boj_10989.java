90bccfe0fe295cfb7c2c632bef93b1cebfb13a34
juhee park <jouhi3322@gmail.com>
2022-07-08 17:20:11 +0900

    카운팅 정렬

--- src/백준/boj_10989.java
+++ src/백준/boj_10989.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class boj_10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         long n = Integer.parseInt(br.readLine());
         int count[] = new int[10001];
 
@@ -17,8 +18,9 @@ public class boj_10989 {
 
         for (int i = 0; i < 10001; i++)
             for (int j = 0; j < count[i]; j++)
-                System.out.println(i);
+                sb.append(i).append("\n");
 
+        System.out.println(sb);
 
     }
 }
