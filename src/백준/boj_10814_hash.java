507cd7c148f38b5dd4855beee7bf14893189a0d6
juhee park <jouhi3322@gmail.com>
2022-07-10 23:06:18 +0900

    using hash

--- src/백준/boj_10814_hash.java
+++ src/백준/boj_10814_hash.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class boj_10814 {
+public class boj_10814_hash {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -14,30 +14,27 @@ public class boj_10814 {
         int n=Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         int arr[]= new int[n];
-        ArrayList<Integer> sortlist=new ArrayList<Integer>();
+        int cpyarr[]= new int[n];
+        HashMap<Integer,Integer> rank = new HashMap<Integer,Integer>();
+
 
         for(int i=0;i<n;i++){
             int s = Integer.parseInt(st.nextToken());
-            arr[i]=s;
-            if(!sortlist.contains(s))sortlist.add(s);
+            arr[i]=cpyarr[i]=s;
         }
 
-        Collections.sort(sortlist);
-        int ansArr[] = new int[n];
-
-
-        for(int i=0;i<sortlist.size();i++){
-            long temp=sortlist.get(i);
-            for (int j = 0; j < n; j++) {
-                    if (temp == arr[j]) {
-                        arr[j] =  (int)Math.pow(10, 10);
-                        ansArr[j] =  i;
-                    }
-                }
+        Arrays.sort(cpyarr);
+        int  ranking=0;
+        for(int i=0;i<cpyarr.length;i++){
+            if(!rank.containsKey(cpyarr[i])){
+                rank.put(cpyarr[i],ranking);
+                ranking++;
             }
+        }
 
-        for(int i:ansArr){
-            sb.append(i).append(' ');
+        for(int i:arr){
+            int rate = rank.get(i);
+            sb.append(rate).append(' ');
         }
 
         System.out.println(sb);
