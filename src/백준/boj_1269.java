e150cf01cfc455183993cc583125079f6f0358ed
juhee park <jouhi3322@gmail.com>
2022-07-11 01:16:33 +0900

    set and hash

---
+++ src/백준/boj_1269.java
@@ -0,0 +1,39 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> hash = new HashSet<Integer>();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            int temp= Integer.parseInt(st.nextToken());
+            hash.add(temp);
+        }
+
+        int cnt1=n;
+        int cnt2=m;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<m;i++){
+            int temp= Integer.parseInt(st.nextToken());
+            if(hash.contains(temp)){
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        sb.append(cnt1+cnt2);
+        System.out.println(sb);
+    }
+}
---
+++ src/백준/boj_1764.java
@@ -0,0 +1,42 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class boj_1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+
+        HashMap<String,Integer> hash = new HashMap<String,Integer>();
+
+        for(int i=0;i<n;i++){
+            String temp = br.readLine();
+            hash.put(temp,0);
+        }
+
+        int cnt=0;
+        ArrayList<String> list= new ArrayList<String>();
+        for(int i=0;i<m;i++){
+            String temp = br.readLine();
+            if(hash.containsKey(temp)){
+                list.add(temp);
+            }
+        }
+
+        Collections.sort(list);
+        sb.append(list.size()).append('\n');
+        for(int i=0;i<list.size();i++)sb.append(list.get(i)+'\n');
+        System.out.println(sb);
+    }
+}
