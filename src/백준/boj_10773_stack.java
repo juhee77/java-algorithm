1c7b09d62bb18a77032c83510703451dd55d79ec
juhee park <jouhi3322@gmail.com>
2022-07-19 01:24:06 +0900

    stack

---
+++ src/백준/boj_10773_stack.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class boj_10773_stack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ;
+
+        int cnt = Integer.parseInt(st.nextToken());
+        Stack<Integer> stack = new Stack<Integer>();
+        int sum = 0;
+
+        for (int i = 0; i < cnt; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) sum -= stack.pop();
+            else {
+                stack.push(input);
+                sum += input;
+            }
+        }
+        sb.append(sum);
+        System.out.println(sb);
+
+    }
+}
---
+++ src/백준/boj_10828_stack.java
@@ -0,0 +1,49 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class boj_10828_stack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());;
+
+        int cnt = Integer.parseInt(st.nextToken());
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i=0;i<cnt;i++){
+
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if(command.equals("push")){
+                int input = Integer.parseInt(st.nextToken());
+                stack.push(input);
+            }
+            else if(command.equals("pop")){
+                if(stack.empty())sb.append("-1").append('\n');
+                else sb.append(stack.pop()).append('\n');
+            }
+            else if(command.equals("size")){
+                sb.append(stack.size()).append('\n');
+            }
+            else if(command.equals("top")){
+                if(stack.empty())sb.append("-1").append('\n');
+                else sb.append(stack.peek()).append('\n');
+            }
+
+            else if(command.equals("empty")){
+                sb.append((stack.empty())?1:0).append('\n');
+            }
+            else{
+                System.out.println("check rule");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
