0faa2d292f33ec62f086cfb99e49d2357221d763
juhee park <jouhi3322@gmail.com>
2022-07-20 01:20:26 +0900

    stack

---
+++ src/백준/boj_108690.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_108690 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int TC = Integer.parseInt(br.readLine());
+        for(int t=0;t<TC;t++){
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
