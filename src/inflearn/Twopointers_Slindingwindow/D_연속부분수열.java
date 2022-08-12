f2db2d54572581a2d78b8a03a52128cfba60200f
juhee park <jouhi3322@gmail.com>
2022-08-12 21:41:29 +0900

    구현

--- src/inflearn/Twopointers_Slindingwindow/D_연속부분수열.java
+++ src/inflearn/Twopointers_Slindingwindow/D_연속부분수열.java
@@ -7,6 +7,20 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class D_연속부분수열 {
+    public int solution(int n, int m, int[] arr){
+        //sum에 lt 부터 rt 까지의합 작으면 rt 가 증가 , 크면 lt+1 하고 앞에 제거하고 뒤에거 추가하면 같는지를 확인
+        int answer=0, sum=0, lt=0; ///lt = left rt=right
+        for(int rt=0; rt<n; rt++){
+            sum+=arr[rt];
+            if(sum==m) answer++;
+            while(sum>=m){
+                sum-=arr[lt++];
+                if(sum==m) answer++;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
