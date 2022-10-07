aa753b81c42a2407f4b096ef380625689768bc20
juhee park <jouhi3322@gmail.com>
2022-10-07 15:41:23 +0900

    스패닝트리 - 1007

---
+++ src/백준/graph/boj_1647_도시분할계획.java
@@ -0,0 +1,83 @@
+package 백준.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class boj_1647_도시분할계획 {
+    private static int v, e, parents[];
+    private static ArrayList<Edge> edges = new ArrayList<>();
+
+    static class Edge implements Comparable<Edge> {
+        private int distance;
+        private int nodeA;
+        private int nodeB;
+
+        public Edge(int distance, int nodeA, int nodeB) {
+            this.distance = distance;
+            this.nodeA = nodeA;
+            this.nodeB = nodeB;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            if (this.distance < o.distance) return -1;
+            else if (this.distance == o.distance) return 0;
+            else return 1;
+        }
+    }
+
+    private static int findParent(int x) {
+        if (x == parents[x]) return x;
+        return parents[x] = findParent(parents[x]);
+    }
+
+    private static void unionParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+        if (a < b) parents[b] = a;
+        else parents[a] = b;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        v = Integer.parseInt(st.nextToken());// 정점
+        e = Integer.parseInt(st.nextToken()); // 간선
+
+        parents = new int[v + 1];
+        // 부모 테이블상에서, 부모를 자기 자신으로 초기화
+        for (int i = 1; i <= v; i++) {
+            parents[i] = i;
+        }
+        //edge 입력
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(c,a,b));
+        }
+        Collections.sort(edges);
+
+        int result = 0;
+        int last = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            int cost = edges.get(i).distance;
+            int a = edges.get(i).nodeA;
+            int b = edges.get(i).nodeB;
+            //사이클이 발생하지 않는 경우에만 집합에 포함
+            if (findParent(a) != findParent(b)) {
+                unionParent(a, b);
+                last = cost;
+                result += cost;
+            }
+        }
+
+        System.out.println(result-last);
+    }
+}
