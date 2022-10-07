c5cecdebad8af5cfdefc0795d8af03f23b4ad004
juhee park <jouhi3322@gmail.com>
2022-10-07 18:38:36 +0900

    stack - 1007

--- src/백준/Stack/boj_17298_stack.java
+++ src/백준/Stack/boj_17298_stack.java
@@ -3,43 +3,33 @@ package 백준.Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class boj_17298_stack {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         int line = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> list = new Stack<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < line; i++) {
-            stack.push(Integer.parseInt(st.nextToken()));
-        }
+        int[] arr = new int[line];
+        int[] ans = new int[line];
 
-        String ans = " ";
         for (int i = 0; i < line; i++) {
-            int temp = stack.pop();
-            if (list.isEmpty()) ans = "-1 " + ans;
-            else {
-                int x = -1;
-                for (int j = list.size() - 1; j >= 0; j--) {
-                    if (list.get(j) > temp) {
-                        x = list.get(j);
-                        break;
-                    }
-                }
-                if (x == -1) ans = "-1 " + ans;
-                else ans = x + " " + ans;
+            int temp = Integer.parseInt(st.nextToken());
+            arr[i] = temp;
+            while (!stack.isEmpty() && temp > arr[stack.peek()]) {
+                ans[stack.pop()] = temp;
             }
-            list.add(temp);
+            stack.push(i);
         }
+        while (!stack.isEmpty()) ans[stack.pop()] = -1;
+        for (int i : ans) sb.append(i + " ");
+        System.out.println(sb);
 
-        System.out.println(ans);
 
     }
 }
