05d133275d12e9beb11010e8e9b1fd290aad2beb
juhee park <jouhi3322@gmail.com>
2022-10-31 23:51:39 +0900

    dp - 1031

--- src/백준/dynamicProgramming/boj_7579_앱_re.java
+++ src/백준/dynamicProgramming/boj_7579_앱_re.java
@@ -3,9 +3,10 @@ package 백준.dynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class boj_7579_앱 {
+public class boj_7579_앱_re {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,24 +23,24 @@ public class boj_7579_앱 {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) pay[i] = Integer.parseInt(st.nextToken());
 
+        int dp[][] = new int[n+1][100001];
 
-        int all[][] = new int[n + 1][100001];
-        int ans = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= 100001; j++) {
-                if (pay[i - 1] <= j) all[i][j] = Math.max(all[i - 1][j - pay[i - 1]] + get[i - 1], all[i - 1][j]);
-                else all[i][j] = all[i - 1][j];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<100001;j++){
+                if(j==0)dp[i][j] = dp[i-1][j];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
 
-                if(all[i][j]>=goal) ans = Math.min(ans,j);
+                if(j>=pay[i-1])dp[i][j]=Math.max(dp[i-1][j-pay[i-1]]+get[i-1],dp[i][j]);
+
             }
         }
 
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 0; j <= 7; j++) {
-//                System.out.print(all[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-        System.out.println(ans);
+        for(int i=0;i<100001;i++){
+            if(goal<=dp[n][i]){
+                System.out.println(i);
+                break;
+            }
+        }
+
     }
 }
