8750a29d3afe051cf7ca016c9e1024ee1d02dc46
juhee park <jouhi3322@gmail.com>
2022-11-06 21:44:37 +0900

    벡트랙킹 - 1106

---
+++ src/백준/backTracking/boj_11561_NM3.java
@@ -0,0 +1,41 @@
+package 백준.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11561_NM3 {
+    private static int n = 0;
+    private static int m = 0;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr=new int[m];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int d) {
+        if (d == m) {
+            for (int e : arr) sb.append(e).append(" ");
+            sb.append("\n");
+
+        } else {
+            for (int i = 1; i <= n; i++) {
+                arr[d] = i;
+                dfs(d + 1);
+            }
+        }
+    }
+}
+
+
+
+
