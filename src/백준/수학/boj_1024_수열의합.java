48655872d2ba34719e1527d0b71bbb5dee941abf
juhee park <jouhi3322@gmail.com>
2022-12-27 20:23:38 +0900

    극한 : 1227

---
+++ src/백준/수학/boj_1024_수열의합.java
@@ -0,0 +1,16 @@
+package 백준.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1024_수열의합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
