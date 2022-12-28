bf3fb9c43d3f9046bedf8b6ffec6b5e76a9f47d0
juhee park <jouhi3322@gmail.com>
2022-12-28 23:35:12 +0900

    극한 : 1227

---
+++ src/백준/수학/boj_26517_극한.java
@@ -0,0 +1,26 @@
+package 백준.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_26517_극한 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        long k = Long.parseLong(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long d = Long.parseLong(st.nextToken());
+        if(a*k+b == c*k+d){
+            System.out.println("Yes "+(a*k+b));
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
