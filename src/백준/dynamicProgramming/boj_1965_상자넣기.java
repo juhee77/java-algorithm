9130e2e65b774f9cafe6f018ebd2245d17f6a702
juhee park <jouhi3322@gmail.com>
2023-01-01 22:10:29 +0900

    dp 긴 문자열 찾기: 0101

--- src/백준/dynamicProgramming/boj_1965_상자넣기.java
+++ src/백준/dynamicProgramming/boj_1965_상자넣기.java
@@ -27,7 +27,24 @@ public class boj_1965_상자넣기 {
             }
             max = Math.max(dp[i],max);
         }
+/*
+        int nums = Integer.parseInt(br.readLine());
+        int[] num = new int[nums+1];
+        st = new StringTokenizer(br.readLine());
+        int[] dp = new int[nums+1];
+        for (int i = 0; i < nums; i++) {
+            num[i+1] = Integer.parseInt(st.nextToken());
+        }
 
+        Arrays.fill(dp,1);
+        for (int i = 1; i <= nums; i++) {
+            for (int j = i; j <= nums; j++) {
+                if(num[i]<num[j] && dp[j]<=dp[i]){
+                    dp[j]=dp[i]+1;
+                }
+            }
+        }
+ */
 //        System.out.println(Arrays.toString(dp));
         System.out.println(max);
     }
