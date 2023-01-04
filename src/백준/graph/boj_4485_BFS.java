ddd802c19177e861a17d9f97ec1ce58f7bcb9725
juhee park <jouhi3322@gmail.com>
2023-01-04 23:44:07 +0900

    bfs풀이 수정 : 0104

--- src/백준/graph/boj_4485_BFS.java
+++ src/백준/graph/boj_4485_BFS.java
@@ -3,16 +3,18 @@ package 백준.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class boj_4485_BFS {
     private static class Point {
-        int x, y, distance;
+        int x, y;
 
-        public Point(int x, int y, int distance) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.distance = distance;
         }
     }
 
@@ -37,39 +39,36 @@ public class boj_4485_BFS {
                 }
             }
 
-            sb.append("Problem ").append(proCount++).append(": ").append(bfs(0,0)).append("\n");
+            sb.append("Problem ").append(proCount++).append(": ").append(bfs(0, 0)).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static int bfs(int startX, int startY){
+    private static int bfs(int startX, int startY) {
         int[][] distance = new int[size][size];
         for (int i = 0; i < size; i++) {
             Arrays.fill(distance[i], INF);
         }
 
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(startX, startY, map[startX][startY]));
+        q.add(new Point(startX, startY));
+        distance[startX][startY] = map[startX][startY];
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Point now = q.poll();
-            //이전에 방문한 경로가 더 적었던 경우 그냥 지나간다.
-            if(distance[now.x][now.y] < now.distance) continue;
-            distance[now.x][now.y] = now.distance;
 
             for (int i = 0; i < 4; i++) {
                 int mvx = now.x + dir[i][0];
                 int mvy = now.y + dir[i][1];
                 if (mvx < 0 || mvy < 0 || size <= mvx || size <= mvy) continue;
 
-                int tempDistance = now.distance + map[mvx][mvy];
+                int tempDistance = distance[now.x][now.y] + map[mvx][mvy];
                 if (tempDistance < distance[mvx][mvy]) {
-                    q.add(new Point(mvx, mvy, tempDistance));
+                    distance[mvx][mvy] = tempDistance;
+                    q.add(new Point(mvx, mvy));
                 }
             }
         }
-
         return distance[size - 1][size - 1];
     }
-
 }
