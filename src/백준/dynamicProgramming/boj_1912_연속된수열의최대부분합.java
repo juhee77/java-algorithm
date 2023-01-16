d92b595bfb6f5785b44665e0172e21b4d0cc92bd
juhee park <jouhi3322@gmail.com>
2023-01-16 21:40:49 +0900

    chore : 0116

--- src/백준/dynamicProgramming/boj_1912_연속된수열의최대부분합.java
+++ src/백준/dynamicProgramming/boj_1912_연속된수열의최대부분합.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj_1912 {
+public class boj_1912_연속된수열의최대부분합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
