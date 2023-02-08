640d19a50bc68874c367071d2af015176f143ec3
juhee park <jouhi3322@gmail.com>
2023-02-08 21:55:22 +0900

    backtracking : 0208

---
+++ src/백준/backTracking/nm/boj_15655_NM6.java
@@ -0,0 +1,53 @@
+package 백준.backTracking.nm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_15655_NM6 {
+    private static boolean[] check;
+    private static int[] arr;
+    private static int[] ans;
+    private static int n,m;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        ans = new int[m];
+        check = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0,0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int start, int nowDepth) {
+        if (nowDepth == m) {
+            for (int num : ans) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if(!check[i]){
+                ans[nowDepth] = arr[i];
+                check[i] = true;
+                dfs(i+1,nowDepth + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
