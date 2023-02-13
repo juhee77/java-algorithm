f2a4a24ddbf7b6422e3041ebdb8d0c22e43e5df0
juhee park <jouhi3322@gmail.com>
2023-02-13 22:33:26 +0900

    greedy+dp : 0213
    refactoring 필요함

--- src/백준/backTracking/boj_1082_방번호.java
+++ src/백준/backTracking/boj_1082_방번호.java
@@ -4,36 +4,102 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class boj_1082_방번호 {
-    private static int n;
-    private static int[] p;
-    private static int goal;
+    private static class Number implements Comparable<Number> {
+        int money;
+        int[] num;
+
+        public Number(int money, int[] num) {
+            this.money = money;
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Number other) {
+
+            int sumA = Arrays.stream(this.num).sum();
+            int sumB = Arrays.stream(other.num).sum();
+            if (sumA == sumB || sumA == this.num[0] || sumB == other.num[0]) {
+
+                for (int i = 9; 0 <= i; i--) {
+                    if (num[i] < other.num[i]) {
+                        return 1;
+                    } else if (num[i] > other.num[i]) {
+                        return -1; // - 나오면 가만 둔다.
+                    }
+                }
+                return other.money - this.money;
+            }
+
+            if (sumA < sumB) return 1;
+            return -1;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        p = new int[n];
+        int[] p = new int[n];
         for (int i = 0; i < n; i++) {
             p[i] = Integer.parseInt(st.nextToken());
         }
-        goal = Integer.parseInt(br.readLine());
+        int goal = Integer.parseInt(br.readLine());
+        int[][] dp = new int[501][10];
+        int[] max = new int[10];
+        PriorityQueue<Number> pq = new PriorityQueue<>();
+        pq.add(new Number(goal, new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
 
-        int[][] dp = new int[n+1][goal+1];
-        for (int i = n; i >= 1; i--) {
-            int now = p[i-1];
-            for (int j = 1; j < goal+1; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                if(j%now ==0 && j>=now){
-                    dp[i][j] = Math.max(j / now, dp[i][j - now] + 1);
+        while (!pq.isEmpty()) {
+            Number poll = pq.poll();
+            // if(dp[poll.money]>poll.num) continue;
+
+            for (int i = n - 1; 0 <= i; i--) {
+                if (poll.money < p[i]) continue;
+                poll.num[i] += 1;
+
+                if (compareNum(dp[poll.money - p[i]], poll.num)) {
+                    poll.num[i] -= 1;
+                    continue;
                 }
+                int[] clone = poll.num.clone();
+                dp[poll.money - p[i]] = clone;
+                pq.add(new Number(poll.money - p[i], dp[poll.money - p[i]]));
+                max = (compareNum(max, poll.num)) ? max : poll.num.clone();
+
+                poll.num[i] -= 1;
             }
-            System.out.println(Arrays.toString(dp[i]));
         }
+
+        if (Arrays.stream(max).sum() == max[0]) {
+            sb.append("0");
+        } else {
+            for (int i = n - 1; 0 <= i; i--) {
+                sb.append(String.valueOf(i).repeat(max[i]));
+            }
+        }
+        System.out.println(sb);
     }
 
+    public static boolean compareNum(int[] a, int[] b) {
+        int sumA = Arrays.stream(a).sum();
+        int sumB = Arrays.stream(b).sum();
+        if (sumA == sumB || sumA == a[0] || sumB == b[0]) {
+            for (int i = 9; 0 <= i; i--) {
+                if (a[i] > b[i]) {
+                    return true;
+                } else if (a[i] < b[i]) {
+                    return false; // - 나오면 가만 둔다.
+                }
+            }
+            return false;
+        }
+        return sumA > sumB;
+    }
 }
