1b107f55dbbde3690cd8e877fc9e175147d3ea73
juhee park <jouhi3322@gmail.com>
2023-02-20 21:29:41 +0900

    수학 : 0220

--- src/백준/수학/boj_1024_수열의합.java
+++ src/백준/수학/boj_1024_수열의합.java
@@ -11,6 +11,31 @@ public class boj_1024_수열의합 {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
 
+
+        // 수열은 x, x+1, x+2, ... x+l-1까지가 최소이므로 x*l + (l)*(l-1)/2 이다.
+        // n = x*l +(l)*(l-1)/2
+        int x = -1;
+        for (; l <= 100; l++) {
+            int temp = (l) * (l - 1) / 2;
+            if (n < temp) {
+                break;
+            } else if (((double) n - temp) % l == 0) {
+                x = (n - temp) / l; // 시작점
+                break;
+            }
+        }
+
+        if (x == -1) {
+            sb.append(-1);
+        } else {
+            for (int i = 0; i < l; i++) {
+                sb.append(x + i).append(" ");
+            }
+        }
+        System.out.println(sb);
     }
 }
