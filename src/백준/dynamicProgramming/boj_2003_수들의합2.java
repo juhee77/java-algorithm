7fa5d416f2c58c846a92864c48846adae67fe4e0
juhee <bora2233@naver.com>
2023-05-09 01:18:25 +0900

    refactor : 0508 - 수들의 합 (dp, subsetSum)
    -부분합

--- src/백준/dynamicProgramming/boj_2003_수들의합2.java
+++ src/백준/dynamicProgramming/boj_2003_수들의합2.java
@@ -8,26 +8,26 @@ import java.util.StringTokenizer;
 public class boj_2003_수들의합2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); //숫자의 개수
+        int m = Integer.parseInt(st.nextToken()); //목표 합
+        long[] subSum = new long[n + 1]; //부분합 저장
+        int startIdx = 0; //시작 위치
 
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        long[] subSum = new long[n + 1];
-        int startIdx = 0;
         st = new StringTokenizer(br.readLine());
         int cnt = 0;
         for (int i = 1; i <= n; i++) {
             int now = Integer.parseInt(st.nextToken());
             subSum[i] = subSum[i - 1] + now;
+
             while (m < subSum[i] - subSum[startIdx]) {
                 startIdx++;
             }
+
             if (m == subSum[i] - subSum[startIdx]) {
                 cnt++;
             }
         }
         System.out.println(cnt);
-
     }
 }
