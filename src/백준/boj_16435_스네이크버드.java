eb0ccaba83bcb0c188143cf16700f4d9d82665f8
juhee77 <bora2233@naver.com>
2023-06-10 22:44:58 +0900

    algo: 0610-구현

---
+++ src/백준/boj_16435_스네이크버드.java
@@ -0,0 +1,31 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_16435_스네이크버드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int height = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= height) {
+                height++;
+            }
+        }
+        System.out.println(height);
+    }
+}
