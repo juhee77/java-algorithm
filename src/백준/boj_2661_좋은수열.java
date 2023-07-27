5378cf01531cc8c8043443ef5c50b27b30e4fb7d
Juhee Park <jouhi3322@gmail.com>
2023-07-27 23:07:22 +0900

    chore : 0727

--- src/백준/boj_2661_좋은수열.java
+++ src/백준/boj_2661_좋은수열.java
@@ -21,32 +21,32 @@ public class boj_2661_좋은수열 {
     }
 
     private static void dfs(int nowDepth) {
-        if (!ans.equals("")) return;
+        if (!ans.equals("")) return; //이미 발견함
 
-        if (nowDepth == goalLen) {
+        if (nowDepth == goalLen) { //발견했다.
             ans = new String(str);
             return;
         }
 
         for (char i = '1'; i <= '3'; i++) {
-            if (nowDepth > 0 && str[nowDepth - 1] == i) { //같은 숫자
-                continue;
-            }
-
             str[nowDepth] = i;
-            boolean flag = true;
-            for (int j = 0; j < nowDepth / 2; j++) {
-                for (int p = 0; p < j; p++) {
-                    if (str[nowDepth - p] == str[nowDepth - j - p]) {
-                        flag = false;
+            boolean flag = true; //우선 넣고 가능한지 확인한다.
+
+            for (int j = 1; j <= (nowDepth + 1) / 2; j++) {
+                //돌아가면서 확인한다. 4배열이라면 [4,3비교] [4,2비교 - 1,3 비교]와 같이 모든 경우 확인
+                flag = false;
+                for (int p = 1; p < (j + 1); p++) {
+                    if (str[nowDepth - p + 1] != str[nowDepth - j - p + 1]) {
+                        flag = true; // 같지 않다 -> 괜찮다.(좋은 수열이다)
                         break;
                     }
                 }
-                if (!flag) break;
-            }
-            if (!flag) continue;
 
-            //nowDepth 이전까지의 모든 idx를 기준으로 겹치지 않아야 한다.
+                if(!flag) break; //괜찮지 않다면(다 똑같다면)-> 나쁜 수열
+            }
+            if (!flag) continue;//나쁜 수열이라면 다음숫자 확인
+
+            //가능한 경우에만 다음것으로 확인
             dfs(nowDepth + 1);
         }
     }
