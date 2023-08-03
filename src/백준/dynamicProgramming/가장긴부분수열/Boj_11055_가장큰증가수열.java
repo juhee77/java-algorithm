59bb0a36ed89c786fa91f41d6dcee4b15c795530
Juhee Park <jouhi3322@gmail.com>
2023-08-03 19:27:28 +0900

    dp : 0803

---
+++ src/백준/dynamicProgramming/가장긴부분수열/Boj_11055_가장큰증가수열.java
@@ -0,0 +1,31 @@
+package 백준.dynamicProgramming.가장긴부분수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_11055_가장큰증가수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int max = 0;
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n];
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+    }
+}
