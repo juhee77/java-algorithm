df56a7376e361b66e3e23c301f60b2899195a4fe
juhee77 <bora2233@naver.com>
2023-12-26 22:19:51 +0900

    정렬 : 1226

---
+++ src/백준/정렬/Boj_11004_K번째수.java
@@ -0,0 +1,30 @@
+package 백준.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj_11004_K번째수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> arr = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(arr);
+        System.out.println(arr.get(k-1));
+
+    }
+}
