d1475c774c057291c01408f188b7103a964664e6
juhee77 <bora2233@naver.com>
2024-01-01 17:12:22 +0900

    string : 0101

---
+++ src/백준/String/Boj_17863_FYI.java
@@ -0,0 +1,18 @@
+package 백준.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_17863_FYI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        if (s.startsWith("555")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
