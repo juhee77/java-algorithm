c1cbf7da48837dd6684a949cf6426b0e66061c54
juhee77 <bora2233@naver.com>
2024-01-21 23:14:36 +0900

    bfs-re : 0121

--- src/백준/bfs/Boj_16920_확장게임.java
+++ src/백준/bfs/Boj_16920_확장게임.java
@@ -4,10 +4,8 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Boj_16920_확장게임 {
     private static int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
@@ -33,8 +31,6 @@ public class Boj_16920_확장게임 {
             ableCnt[i] = Integer.parseInt(st.nextToken());
         }
 
-        int total = 0;
-        int goal = n * m;
         for (int i = 0; i < n; i++) {
             char[] inputs = br.readLine().toCharArray();
             for (int j = 0; j < m; j++) {
@@ -43,10 +39,6 @@ public class Boj_16920_확장게임 {
                     int index = inputs[j] - '1';
                     castles.get(index).add(new Point(i, j));
                     ans[index]++;
-                    total++;
-                }
-                if (inputs[j] == '#') {
-                    goal--;
                 }
             }
         }
@@ -55,25 +47,25 @@ public class Boj_16920_확장게임 {
 //        System.out.println("ANS" + Arrays.toString(ans));
 
 
-        //각자 얻는 성의 수
-        boolean[][] isExpended = new boolean[n][m];
-        while (true) {
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            HashSet<Point> hashSet = new HashSet<>();
             for (int i = 0; i < player; i++) {
                 //한 플레이어의 성의 사이즈
                 int size = castles.get(i).size();
+                hashSet.clear();
 
                 for (int j = 0; j < size; j++) { //성의 개수 만큼
                     Point castle = castles.get(i).poll();
                     Queue<Point> next = new ArrayDeque<>(); //성의 개수 만큼 확장
                     next.add(castle);
-                    isExpended[castle.x][castle.y] = true;
 
                     for (int p = 0; p < ableCnt[i] && !next.isEmpty(); p++) {
                         int nowSize = next.size();
 
                         for (int k = 0; k < nowSize; k++) {
                             Point poll = next.poll();
-                            isExpended[poll.x][poll.y] = true;
 
                             for (int[] dir : dirs) {
                                 int mvx = dir[0] + poll.x;
@@ -82,27 +74,24 @@ public class Boj_16920_확장게임 {
                                 if (mvx < 0 || n <= mvx || mvy < 0 || m <= mvy) {
                                     continue;
                                 }
-                                if (map[mvx][mvy] == '.' || ((map[mvx][mvy] == (char) (i + '1') && !isExpended[mvx][mvy]))) {
+                                if (map[mvx][mvy] == '.') {
                                     next.add(new Point(mvx, mvy));
-                                    isExpended[mvx][mvy] = true;
-                                    if (map[mvx][mvy] == '.') {
-                                        castles.get(i).add(new Point(mvx, mvy));
-                                        ans[i]++;
-                                        total++;
-                                        map[mvx][mvy] = (char) (i + '1');
-                                    }
+                                    castles.get(i).add(new Point(mvx, mvy));
+                                    hashSet.add(new Point(mvx, mvy));
                                 }
                             }
                         }
                     }
                 }
+
+                if (hashSet.size() > 0) {
+                    flag = true;
+                    ans[i]+=hashSet.size();
+                }
+                for (Point po : hashSet) {
+                    map[po.x][po.y] = (char) (i + '1');
+                }
             }
-
-            if (total == goal) {
-                break;
-            }
-
-
         }
 
         for (int an : ans) {
