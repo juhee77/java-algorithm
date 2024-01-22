486c5e022255d3239426a4c723378cb640a6f502
juhee77 <bora2233@naver.com>
2024-01-22 23:33:55 +0900

    basic : 0122

---
+++ src/백준/basic/Boj_25314_코딩은체육과목입니다.java
@@ -0,0 +1,17 @@
+package 백준.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_25314_코딩은체육과목입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        sb.append("long ".repeat(n / 4)).append("int ");
+        System.out.println(sb);
+    }
+}
