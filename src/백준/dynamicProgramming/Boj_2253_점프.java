07a2e4fbab7ddef0edf551d2f6adc46ecc1e6224
juhee77 <bora2233@naver.com>
2024-02-14 00:35:53 +0900

    dp : 0213

--- src/백준/dynamicProgramming/Boj_2253_점프.java
+++ src/백준/dynamicProgramming/Boj_2253_점프.java
@@ -7,19 +7,18 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj_2253_점프 {
-    private static int MAX = (int) 100000;
-    private static int MMAX = (int) 200000;
+    private final static int MAX = 10000000;
+    private final static int MMAX = 20000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int v = (int)Math.sqrt((n + 1));
-        int[][] dp = new int[n + 1][v+1];
+        int v = (int) Math.sqrt(2 * n);
+        int[][] dp = new int[n + 1][v + 2];
         for (int i = 0; i <= n; i++) {
             Arrays.fill(dp[i], MAX);
         }
@@ -28,32 +27,23 @@ public class Boj_2253_점프 {
             Arrays.fill(dp[now], MMAX);
         }
 
-        dp[1][1] = 1;
+        dp[1][0] = 0;
         //현재칸에서 지금 건너갈 수 있는 range 칸일때 점프 횟수
-        for (int i = 0; i < n ; i++) {
-            for (int j = 1; j <= v; j++) {
-                if (dp[i][j] != MAX && dp[i][j] != MMAX) {
-                    if (i + j + 1 <= v  && dp[i + j + 1][j + 1] != MMAX) {
-                        dp[i + j + 1][j + 1] = Math.min(dp[i + j + 1][j + 1], dp[i][j] + 1);
-                    }
-                    if (i + j <= v  && dp[i + j][j] != MMAX) {
-                        dp[i + j][j] = Math.min(dp[i + j][j], dp[i][j] + 1);
-                    }
-                    if (i + j - 1 <= v  && dp[i + j - 1][j - 1] != MMAX) {
-                        dp[i + j - 1][j - 1] = Math.min(dp[i + j - 1][j - 1], dp[i][j] + 1);
-                    }
-                }
+        for (int i = 2; i <= n; i++) {
+            if (dp[i][0] == MMAX) continue;
+
+            for (int j = 1; j < Math.sqrt(2 * i); j++) {
+                dp[i][j] = Math.min(Math.min(dp[i - j][j - 1], dp[i - j][j]), dp[i - j][j + 1]) + 1;
             }
 
         }
 
         int ans = MAX;
         for (int i = 1; i <= v; i++) {
-            System.out.println(Arrays.toString(dp[i]));
-            ans = Math.min(ans, dp[v][i]);
+            ans = Math.min(ans, dp[n][i]);
         }
 
-        System.out.println(ans == MAX || ans == 0 ? -1 : ans - 1);
+        System.out.println(ans == MAX ? -1 : ans);
 
     }
 }
