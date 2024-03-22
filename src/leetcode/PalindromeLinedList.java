1c8d80d7187071b734f48a2a12298b19817d54bb
Juhee Park <jouhi3322@gmail.com>
2024-03-23 00:11:59 +0900

    linkedList : 0322

--- src/leetcode/PalindromeLinedList.java
+++ src/leetcode/PalindromeLinedList.java
@@ -11,14 +11,37 @@ public class PalindromeLinedList {
      * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
+//    public boolean isPalindrome(ListNode head) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        while (head != null) {
+//            sb.append(head.val);
+//            head = head.next;
+//        }
+//
+//        return sb.toString().contentEquals(sb.reverse());
+//    }
     public boolean isPalindrome(ListNode head) {
         StringBuilder sb = new StringBuilder();
 
-        while (head != null) {
-            sb.append(head.val);
-            head = head.next;
+        ListNode tail = head;
+        ListNode now = new ListNode(0);
+        int cnt = 0;
+        while (tail != null) {
+            cnt++;
+            ListNode hold = now;
+            now = new ListNode(tail.val);
+            now.next = hold;
+
+            tail = tail.next;
         }
 
-        return sb.toString().contentEquals(sb.reverse());
+        for (int i = 0; i <= cnt / 2; i++) {
+            if (head.val != now.val) return false;
+
+            now = now.next;
+            head = head.next;
+        }
+        return true;
     }
 }
