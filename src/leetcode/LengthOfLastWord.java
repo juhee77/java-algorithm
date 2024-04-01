83f808384504464d67d52f10e2882a1b5c3b6d37
juhee77 <bora2233@naver.com>
2024-04-01 21:56:23 +0900

    string : 0401

---
+++ src/leetcode/LengthOfLastWord.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] split = s.split(" ");
+        return split[split.length - 1].length();
+    }
+}
