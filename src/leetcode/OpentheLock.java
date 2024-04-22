6823c577b89b81a0600e19a8fe8a8d73a23915b4
juhee77 <bora2233@naver.com>
2024-04-22 23:00:38 +0900

    dfs : 0422

---
+++ src/leetcode/OpentheLock.java
@@ -0,0 +1,61 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class OpentheLock {
+    public int openLock(String[] deadends, String target) {
+        Queue<Integer> bfs = new ArrayDeque<>();
+        boolean[] visited = new boolean[10000];
+
+        for (String deadend : deadends) {
+            visited[Integer.valueOf(deadend)] = true;
+        }
+
+        bfs.add(0);
+        visited[0] = true;
+        int n = 0;
+
+        while (!bfs.isEmpty()) {
+            int size = bfs.size();
+            while (size-- > 0) {
+                Integer poll = bfs.poll();
+                System.out.println(poll);
+                if (poll == Integer.parseInt(target)) {
+                    return n;
+                }
+
+                for (int i = 1; i <= 1000; i *= 10) {
+                    // 1 더하기
+                    int range = (poll / i) % 10 + 1;
+                    int hold = 0;
+                    if (range == 10) {
+                        hold = -9 * i;
+                    }else{
+                        hold = i;
+                    }
+
+                    int newVal = poll + hold;
+                    if (0<=newVal && newVal < 10000 && !visited[newVal]) {
+                        bfs.add(newVal);
+                        visited[newVal] = true;
+                    }
+
+                    // 1 빼기
+                    range = (poll / i) % 10 - 1;
+                    if (range == -1) {
+                        hold =  9 * i;
+                    }else{
+                        hold = -i;
+                    }
+
+                    newVal = poll + hold;
+                    if (0<=newVal && newVal < 10000 && !visited[newVal]) {
+                        bfs.add(newVal);
+                        visited[newVal] = true;
+                    }
+                }
+            }
+            n++;
+        }
+        return -1;
+    }
+}
