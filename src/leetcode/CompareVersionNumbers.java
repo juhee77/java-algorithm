cc55eb318357e4036765af5279e7ae54bb32b01b
Juhee Park <jouhi3322@gmail.com>
2024-05-04 00:07:37 +0900

    compare : 0504

--- src/leetcode/CompareVersionNumbers.java
+++ src/leetcode/CompareVersionNumbers.java
@@ -5,22 +5,27 @@ class CompareVersionNumbers {
         String[] split1 = version1.split("\\.");
         String[] split2 = version2.split("\\.");
 
-        int hold1_1 = Integer.parseInt(split1[0]);
-        int hold1_2 = Integer.parseInt(split1[0]);
-        int hold2_1 = Integer.parseInt(split2[0]);
-        int hold2_2 = Integer.parseInt(split2[0]);
+        int[] num1 = new int[split1.length];
+        int[] num2 = new int[split2.length];
 
-        if (hold1_1 > hold2_1) {
-            return 1;
-        } else if (hold1_1 < hold2_1) {
-            return -1;
-        }else{
-            if (hold1_2 > hold2_2) {
+        for (int i = 0; i < num1.length; i++) {
+            num1[i] = Integer.parseInt(split1[i]);
+        }
+        for (int i = 0; i < num2.length; i++) {
+            num2[i] = Integer.parseInt(split2[i]);
+        }
+
+        for (int i = 0; i < Math.max(num1.length, num2.length); i++) {
+
+            int left = (num1.length > i) ? num1[i] : 0;
+            int right = (num2.length > i) ? num2[i] : 0;
+
+            if (left > right) {
                 return 1;
-            } else if (hold1_2 < hold2_2) {
+            } else if (left < right) {
                 return -1;
             }
-            return 0;
         }
+        return 0;
     }
 }
