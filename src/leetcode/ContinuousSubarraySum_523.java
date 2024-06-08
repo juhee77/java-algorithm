4ba211f664e73a5acbcf237934ba3dc2bd887fd2
Juhee Park <jouhi3322@gmail.com>
2024-06-09 00:11:07 +0900

    set : 0608

--- src/leetcode/ContinuousSubarraySum_523.java
+++ src/leetcode/ContinuousSubarraySum_523.java
@@ -10,15 +10,22 @@ class ContinuousSubarraySum_523 {
         for (int i = 0; i < nums.length; i++) {
             hold = (hold + nums[i] % k) % k;
             if (!map.containsKey(hold)) map.put(hold, i);
-
-//            if (i >= 2 && hold % k == 0) return true;
+            if (i >= 1 && hold % k == 0) return true;
 
             if (map.containsKey(hold)) {
                 Integer tempIndex = map.get(hold);
-                if (i - tempIndex >= 2) return true;
+                if (i - tempIndex >= 1) return true;
             }
         }
         return false;
     }
 
+    public static void main(String[] args) {
+        ContinuousSubarraySum_523 continuousSubarraySum523 = new ContinuousSubarraySum_523();
+        System.out.println(continuousSubarraySum523.checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6));
+        System.out.println(continuousSubarraySum523.checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 6));
+        System.out.println(continuousSubarraySum523.checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 13));
+        System.out.println( continuousSubarraySum523.checkSubarraySum(new int[]{23,2,4,6,6}, 7));
+        System.out.println( continuousSubarraySum523.checkSubarraySum(new int[]{0}, 1));
+    }
 }
