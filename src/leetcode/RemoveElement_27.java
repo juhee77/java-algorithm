567816df2dfa5b0298ee79964654b5660c9044e6
juhee77 <bora2233@naver.com>
2024-06-14 01:39:43 +0900

    two pointer : 0613

--- src/leetcode/RemoveElement_27.java
+++ src/leetcode/RemoveElement_27.java
@@ -1,25 +1,24 @@
 package leetcode;
 
-import java.util.Arrays;
-
 class RemoveElement_27 {
     public int removeElement(int[] nums, int val) {
-        int left=0,right=nums.length-1;
+        int left = 0, right = nums.length - 1;
         int cnt = 0;
-        while(left<right && 0<=right){
-            while(0<=right && nums[right]==val){
+
+        while (left <= right && 0 <= right) {
+            while (0 <= right && nums[right] == val) {
+                right--;
+            }
+            if (left > right)
+                break;
+
+            if (nums[left] == val) {
+                nums[left] = nums[right];
                 right--;
             }
 
-            if(nums[left]==val){
-                nums[left] = nums[right];
-                nums[right] =-1;
-                cnt++;
-            }
-            
+            cnt++;
             left++;
-            right--;
-            System.out.println(left+" "+right+" " + Arrays.toString(nums));
         }
         return cnt;
     }
