43b417c1df9d9589d205216f0b3e03f37588df8c
juhee77 <bora2233@naver.com>
2024-07-25 00:23:01 +0900

    sort : 0724

--- src/leetcode/SorttheJumbledNumbers_2191.java
+++ src/leetcode/SorttheJumbledNumbers_2191.java
@@ -1,7 +1,43 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class SorttheJumbledNumbers_2191 {
+    private static class Number implements Comparable<Number> {
+        long original;
+        long changed;
+
+        public Number(long original, long changed) {
+            this.original = original;
+            this.changed = changed;
+        }
+
+        @Override
+        public int compareTo(Number o) {
+            return Long.compare(changed, o.changed);
+        }
+    }
+
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        
+        List<Number> numbers = new ArrayList<>();
+        for (int num : nums) {
+            long changed = 0;
+            String s = Integer.toString(num);
+            for (char c : s.toCharArray()) {
+                changed += mapping[c - '0'];
+                changed *= 10;
+            }
+
+            numbers.add(new Number(num, changed));
+        }
+
+        Collections.sort(numbers);
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) numbers.get(i).original;
+        }
+        return ans;
     }
 }
