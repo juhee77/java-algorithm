fa673fbd09681a4bd67e2960456d672f01deea19
Juhee Park <jouhi3322@gmail.com>
2024-08-12 05:11:07 +0900

    math : 0812

--- src/백준/dfs/Boj_10972_다음순열.java
+++ src/백준/dfs/Boj_10972_다음순열.java
@@ -6,86 +6,60 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj_10972_다음순열 {
-    private static int n;
-    private static int[] ans;
-    private static int[] returnAns;
-    private static int[] now;
-    private static boolean[] visited;
-    private static boolean flag = false, find = false;
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        ans = new int[n];
+        int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
             ans[i] = Integer.parseInt(st.nextToken());
         }
 
-        now = new int[n];
-        visited = new boolean[n];
-        returnAns = new int[n];
-
-        int point = n;
-        for (int i = 0; i < n; i++) {
-            if (ans[i] == point) {
-                now[i] = point;
-                visited[point - 1] = true;
-                point--;
-            } else {
-                break;
+        if (nextPermutation(n, ans)) {
+            for (int i = 0; i < n; i++) {
+                sb.append(ans[i]).append(" ");
             }
-        }
-
-        System.out.println(n - point);
-        dfs(n - point);
-
-        if (!find || point == 0) {
-            System.out.println(-1);
+            System.out.println(sb.toString().trim());
         } else {
-            for (int returnAn : returnAns) {
-                sb.append(returnAn).append(" ");
-            }
-            System.out.println(sb);
+            System.out.println(-1);
         }
     }
 
-    private static void dfs(int depth) {
-        if (depth == n) {
-            if (flag) {
-                returnAns = now;
-                find = true;
-                return;
-            }
-            if (isSame(ans, now)) {
-                flag = true;
-            }
-            return;
+    private static boolean nextPermutation(int n, int[] ans) {
+        int left = ans.length - 1;
+        while (left > 0 && ans[left - 1] >= ans[left]) {
+            left--;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i - 1]) {
-                visited[i - 1] = true;
-                now[depth] = i;
-                dfs(depth + 1);
-                if (find) return;
-                visited[i - 1] = false;
-            }
+        if (left == 0) return false;
+
+        int right = ans.length - 1;
+        while (ans[right] <= ans[left - 1]) {
+            right--;
         }
 
-    }
-
-    private static boolean isSame(int[] arr1, int[] arr2) {
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
+        swap(ans, left - 1, right);
+        reverse(ans, left, n - 1);
         return true;
     }
+
+
+    private static void reverse(int[] ans, int left, int right) {
+        while (left < right) {
+            swap(ans, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private static void swap(int[] ans, int i, int j) {
+        int temp = ans[i];
+        ans[i] = ans[j];
+        ans[j] = temp;
+    }
+
 }
