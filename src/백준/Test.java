bad22bbb42e087d6fb35ca01b60de4485872df3a
Juhee Park <jouhi3322@gmail.com>
2024-09-13 23:59:45 +0900

    basic : 0913

---
+++ src/백준/Test.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+    }
+}
