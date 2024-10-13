1da905b557012c3908ddd562424ddc697ec60c2b
Juhee Park <jouhi3322@gmail.com>
2024-10-13 22:21:55 +0900

    fix : 1013

--- src/리트코드/ContainsDuplicate.java
+++ src/리트코드/ContainsDuplicate.java
@@ -1,5 +1,5 @@
 package 리트코드;
-import java.awt.image.AreaAveragingScaleFilter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,27 +7,29 @@ public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums) {
 
         ArrayList<Integer> list = new ArrayList();
-        for(int i=0;i< nums.length;i++){
-            if(list.contains(nums[i]))
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i]))
                 return true;
             list.add(nums[i]);
         }
         return false;
     }
-    public static void main(String args[]){
-        int[] a={1,2,3,1};
+
+    public static void main(String args[]) {
+        int[] a = {1, 2, 3, 1};
         System.out.print(containsDuplicate(a));
     }
-}
-///위에 복잡도 문제로 시간더 오래걸림
-//아래처럼 하는게 시간이 더 적게걸림
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i< nums.length;i++){
-            if(nums[i]==nums[i-1])
-                return true;
+
+    ///위에 복잡도 문제로 시간더 오래걸림
+    //아래처럼 하는게 시간이 더 적게걸림
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i - 1])
+                    return true;
+            }
+            return false;
         }
-        return false;
     }
 }
