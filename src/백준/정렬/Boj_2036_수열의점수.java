7f60afa455201a6fde8e715cb1f9683b73f7a745
juhee77 <bora2233@naver.com>
2024-10-23 00:01:20 +0900

    정렬 : 1022

--- src/백준/정렬/Boj_2036_수열의점수.java
+++ src/백준/정렬/Boj_2036_수열의점수.java
@@ -15,29 +15,41 @@ public class Boj_2036_수열의점수 {
         int n = Integer.parseInt(br.readLine());
         List<BigInteger> minusArr = new ArrayList<>();
         List<BigInteger> plusArr = new ArrayList<>();
+        boolean hasZero = false;
+        BigInteger ones = BigInteger.ZERO;
 
         while (n-- > 0) {
             BigInteger value = new BigInteger(br.readLine());
             if (value.compareTo(BigInteger.ZERO) > 0) {
-                plusArr.add(value);
+                if (value.equals(BigInteger.ONE)) {
+                    ones = ones.add(BigInteger.ONE);
+                } else {
+                    plusArr.add(value);
+                }
             } else if (value.compareTo(BigInteger.ZERO) < 0) {
                 minusArr.add(value);
+            } else {
+                hasZero = true;
             }
         }
 
         Collections.sort(minusArr);
         plusArr.sort(Collections.reverseOrder());
-        BigInteger ans = BigInteger.ZERO;
-        ans = ans.add(getMaxScore(minusArr));
-        ans = ans.add(getMaxScore(plusArr));
+
+        BigInteger ans = ones;
+        ans = ans.add(getMaxScore(minusArr, hasZero));
+        ans = ans.add(getMaxScore(plusArr, false));
 
         System.out.println(ans);
     }
 
-    private static BigInteger getMaxScore(List<BigInteger> arr) {
+    private static BigInteger getMaxScore(List<BigInteger> arr, boolean hasZero) {
         BigInteger ans = BigInteger.ZERO;
         for (int i = 0; i < arr.size(); i += 2) {
             if (i == arr.size() - 1) {
+                if (hasZero && arr.get(i).compareTo(BigInteger.ZERO) < 0) {
+                    continue;
+                }
                 ans = ans.add(arr.get(i));
             } else {
                 ans = ans.add(arr.get(i).multiply(arr.get(i + 1)));
