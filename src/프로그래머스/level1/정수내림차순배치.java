d759b0de1fb680d2071724477eceac3659d9d7b5
juhee77 <bora2233@naver.com>
2024-10-25 20:30:12 +0900

    chore : 1025

--- src/프로그래머스/level1/정수내림차순배치.java
+++ src/프로그래머스/level1/정수내림차순배치.java
@@ -1,9 +1,6 @@
 package 프로그래머스.level1;
 
-import org.apache.groovy.util.SystemUtil;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,12 +8,13 @@ import java.util.stream.Collectors;
 public class 정수내림차순배치 {
     public long solution(long n) {
         List a = String.valueOf(n).chars().map(ch -> ch - '0').boxed().collect(Collectors.toList());
-        Collections.sort(a,Collections.reverseOrder());
-        String answer = a.toString().replaceAll("\\[|\\]","").replaceAll(",| ","");
+        Collections.sort(a, Collections.reverseOrder());
+        String answer = a.toString().replaceAll("\\[|\\]", "").replaceAll(",| ", "");
         //System.out.println(a.toString().replaceAll("\\[|\\]","").replaceAll(",| ",""));
         return Long.parseLong(answer);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         정수내림차순배치 a = new 정수내림차순배치();
         System.out.println(a.solution(118372));
     }
--- src/프로그래머스/level2/후보키.java
+++ src/프로그래머스/level2/후보키.java
@@ -1,33 +1,31 @@
 package 프로그래머스.level2;
 
-import org.apache.groovy.contracts.common.impl.lc.PreconditionLifecycle;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 
 public class 후보키 {
     static int[] arr;
     static boolean[] visit;
     static String[][] Arelation;
     static boolean flag;
-    int cnt =0;
-    private static void dfs(int n, int m, int d,int f) {
-        if(d == m) {
+    int cnt = 0;
+
+    private static void dfs(int n, int m, int d, int f) {
+        if (d == m) {
 //            String temp="";
 //            for(int a : arr) temp += a+" ";
 //            System.out.println(temp);
 //            //고유키 분류가 가능한지 체크
-            if(check()) flag = true;
+            if (check()) flag = true;
             else flag = false;
             return;
         }
 
-        for(int i = f; i < n; i++) {
-            if(!visit[i]) {
+        for (int i = f; i < n; i++) {
+            if (!visit[i]) {
                 visit[i] = true;
                 arr[d] = i;
-                dfs(n, m, d+1,i);
+                dfs(n, m, d + 1, i);
                 visit[i] = false;
             }
         }
@@ -35,8 +33,8 @@ public class 후보키 {
     }
 
 
-    public static boolean check(){
-        int d= arr.length;
+    public static boolean check() {
+        int d = arr.length;
 //        HashSet<String[]> hash = new HashSet<>();
 //
 //        for(int i=0;i< Arelation.length;i++){
@@ -59,17 +57,17 @@ public class 후보키 {
 //        return true;
         HashSet<String> hash = new HashSet<>();
 
-        for(int i=0;i< Arelation.length;i++){
+        for (int i = 0; i < Arelation.length; i++) {
             String reltemp[] = Arelation[i];
             String temp[] = new String[d];
-            for(int j=0;j<arr.length;j++) temp[j] = reltemp[arr[j]];
-            String temparr="";
-            for(int j=0;j<temp.length;j++){
-                temparr += temp[j]+" ";
+            for (int j = 0; j < arr.length; j++) temp[j] = reltemp[arr[j]];
+            String temparr = "";
+            for (int j = 0; j < temp.length; j++) {
+                temparr += temp[j] + " ";
 //                if(j<temp.length) {
 //                    if(hash.contains(temparr)
 //                }
-                if(hash.contains(temparr)) return false;
+                if (hash.contains(temparr)) return false;
                 hash.add(temparr);
             }
 
@@ -83,23 +81,24 @@ public class 후보키 {
     }
 
     public int solution(String[][] relation) {
-        Arelation= relation;
+        Arelation = relation;
         int max = relation[0].length;
-        for(int i=1;i<=max;i++){
+        for (int i = 1; i <= max; i++) {
             //i개를 뽑는 경우의 수
-            arr = new int[i]; visit = new boolean[max];
-            dfs(max,i, 0,0);
+            arr = new int[i];
+            visit = new boolean[max];
+            dfs(max, i, 0, 0);
 
-            if(flag) continue;
-            else return (i==max)?0:i;
+            if (flag) continue;
+            else return (i == max) ? 0 : i;
         }
         //흐보키 없는 경우
         return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(new 후보키().solution(new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}}));
-        System.out.println(new 후보키().solution(new String[][]{{"100","ryan","music","2"},{"100","ryan","music","2"}}));
+        System.out.println(new 후보키().solution(new String[][]{{"100", "ryan", "music", "2"}, {"200", "apeach", "math", "2"}, {"300", "tube", "computer", "3"}, {"400", "con", "computer", "4"}, {"500", "muzi", "music", "3"}, {"600", "apeach", "music", "2"}}));
+        System.out.println(new 후보키().solution(new String[][]{{"100", "ryan", "music", "2"}, {"100", "ryan", "music", "2"}}));
     }
 
 }
