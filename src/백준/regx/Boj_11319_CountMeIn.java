ae54e8f4aeb378e6e6d8b2b9a315c6407d98c9c4
juhee77 <bora2233@naver.com>
2024-11-28 00:03:11 +0900

    basic : 1127

--- src/백준/regx/Boj_11319_CountMeIn.java
+++ src/백준/regx/Boj_11319_CountMeIn.java
@@ -4,22 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Boj_11319_CountMeIn {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        HashSet<Character> characters = new HashSet<>();
-        characters.add('a','e','i','o','u','y')
+        HashSet<Character> vowelsSet = new HashSet<>(List.of('A','E','I','O','U','a','e','i','o','u'));
         int n = Integer.parseInt(br.readLine());
         while (n-- > 0) {
+            int vowels = 0;
+            int consonants = 0;
             char[] chars = br.readLine().toCharArray();
             for (char aChar : chars) {
-                if(aChar == 'a')
+                if(aChar==' ')continue;
+                if (vowelsSet.contains(aChar)) vowels++;
+                else consonants++;
             }
+            sb.append(consonants + " " + vowels).append("\n");
         }
+        System.out.println(sb);
     }
 }
