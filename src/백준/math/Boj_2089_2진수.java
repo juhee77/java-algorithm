723124df1c4f3714859ae0e9ea7711eb4e1d477c
juhee77 <bora2233@naver.com>
2025-01-09 23:59:45 +0900

    math : 0109

---
+++ src/백준/math/Boj_2089_2진수.java
@@ -0,0 +1,26 @@
+package 백준.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2089_2진수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n != 0) {
+            int remainder = n % -2;
+            n /= -2;
+
+            if (remainder < 0) {
+                remainder += 2;
+                n += 1;
+            }
+
+            sb.append(remainder);
+        }
+        System.out.println(sb.reverse().toString());
+    }
+}
