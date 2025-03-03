f76dc30b72a54c704aee3ca26fe37e93d53dc903
Juhee Park <jouhi3322@gmail.com>
2025-03-04 00:23:01 +0900

    basic : 0304

---
+++ src/백준/basic/Boj_9195_주사위.java
@@ -0,0 +1,25 @@
+package 백준.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_9195_주사위 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int i = 1;
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(String.format("Case %d: %d\n", i++, (a + b)));
+        }
+        System.out.println(sb);
+    }
+}
