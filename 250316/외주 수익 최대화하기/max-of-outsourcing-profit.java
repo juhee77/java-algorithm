d3a9c410beddcb8a03e322929b7f1033c3cc93b1
Juhee Park <51548333+juhee77@users.noreply.github.com>
2025-03-16 16:49:48 +0900

    Title: 외주 수익 최대화하기, Time: 84ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250316/외주 수익 최대화하기/max-of-outsourcing-profit.java
+++ 250316/외주 수익 최대화하기/max-of-outsourcing-profit.java
@@ -10,50 +10,6 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-//        PriorityQueue<int[]> pq = new PriorityQueue<>(
-////                new Comparator<int[]>() {
-////                    @Override
-////                    public int compare(int[] o1, int[] o2) {
-////                        if (o1[1] == o2[1]) {
-////                            if (o1[2] == o2[2]) {
-////                                return o1[0] - o2[0]; //먼저 시작하는거
-////                            }
-////                            return o2[2] - o1[2]; // 더 돈 많이 주는거
-////                        }
-////                        return o1[1] - o2[1]; //더 빨리 끝나는게 먼저
-////                    }
-////                }
-////        );
-////        int n = Integer.parseInt(br.readLine());
-////
-////        int day = 0;
-////        while (n-- > 0) {
-////            st = new StringTokenizer(br.readLine());
-////            int end = Integer.parseInt(st.nextToken()) + day;
-////            int cost = Integer.parseInt(st.nextToken());
-////            pq.add(new int[]{day, end, cost});
-////            day++;
-////        }
-////
-////        int nowDay = 0;
-////        int cnt = 0;
-////        while (!pq.isEmpty()) {
-////            int[] poll = pq.poll();
-//////            System.out.println(Arrays.toString(poll));
-////            if (nowDay <= poll[0]) {
-////                nowDay = poll[1];
-////                cnt += poll[2];
-////            }
-////
-////            if (poll[2] > cnt) {
-////                cnt = poll[2];
-////                nowDay = poll[1];
-////            }
-////        }
-////
-////        System.out.println(cnt);
-
-
         int n = Integer.parseInt(br.readLine());
         int[][] dp = new int[20][20];
         for (int i = 0; i < n; i++) {
