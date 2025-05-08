2da1372acb3ea0f93f3378c09f79bbcbae28b229
juhee77 <bora2233@naver.com>
2025-05-08 23:44:49 +0900

    구현 : 0508

--- src/백준/구현/Boj_7014_SnowCones.java
+++ src/백준/구현/Boj_7014_SnowCones.java
@@ -3,7 +3,7 @@ package 백준.구현;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Boj_7014_SnowCones {
@@ -13,63 +13,51 @@ public class Boj_7014_SnowCones {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            char[] input = br.readLine().toCharArray();
-            char[] output = br.readLine().toCharArray();
+        for (int TC = 1; TC <= n; TC++) {
+            char[] curr = br.readLine().toCharArray();
+            char[] goal = br.readLine().toCharArray();
 
-            int[] leftO = new int[input.length];
-            int[] rightO = new int[input.length];
-            int[] leftX = new int[input.length];
-            int[] rightX = new int[input.length];
+            ArrayList<Integer> currXPos = new ArrayList<Integer>();
+            ArrayList<Integer> goalXPos = new ArrayList<Integer>();
+            for (int i = 0; i < curr.length; i++) {
+                if (curr[i] == 'X')
+                    currXPos.add(i);
+                if (goal[i] == 'X')
+                    goalXPos.add(i);
+            }
 
-            left(input, 'O', leftO);
-            left(input, 'X', leftX);
-
-            right(input, 'O', rightO);
-            right(input, 'X', rightX);
-
-//            System.out.println(Arrays.toString(leftO) + Arrays.toString(rightO) + Arrays.toString(leftX) + Arrays.toString(rightX));
-            sb.append("Data Set " + i + ":" + "\n");
-            int left = findMinMax(leftO, 'O', rightO, output);
-            int right = findMinMax(leftX, 'X', rightX, output);
-//            System.out.println(left + "," + right);
-            sb.append(Math.max(left, right)).append("\n");
-            sb.append("\n");
+            int numX = currXPos.size();
+            int numSwaps = 0;
+            boolean same = false;
+            while (!same) {
+                same = true;
+                //왼쪽부터 스캔
+                for (int i = 0; i < numX; i++) {
+                    int currXInd = currXPos.get(i);
+                    if (currXInd < goalXPos.get(i)) // need to move right
+                    {
+                        if (!currXPos.contains(currXInd + 1)) {
+                            currXPos.set(i, currXInd + 1);
+                        }
+                        same = false;
+                    }
+                }
+                //오른쪽 부터 스캔
+                for (int i = numX - 1; i >= 0; i--) {
+                    int currXInd = currXPos.get(i);
+                    if (currXPos.get(i) > goalXPos.get(i)) {
+                        if (!currXPos.contains(currXInd - 1)) {
+                            currXPos.set(i, currXInd - 1);
+                        }
+                        same = false;
+                    }
+                }
+                if (!same)
+                    numSwaps++;
+            }
+            sb.append("Data Set " + TC + ":\n" + numSwaps + "\n\n");
+            //System.out.println("" + maxBound); // debugging only
         }
         System.out.println(sb);
-    }
-
-    private static int findMinMax(int[] leftO, char hold, int[] rightO, char[] goal) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < leftO.length; i++) {
-            if (goal[i] == hold)
-                min = Math.max(leftO[i], rightO[i]);
-        }
-        return min;
-    }
-
-    private static void left(char[] input, char hold, int[] leftO) {
-        int leftIdx = Integer.MAX_VALUE;
-        for (int j = 0; j < input.length; j++) {
-            if (input[j] == hold)
-                leftIdx = j;
-            if (leftIdx == Integer.MAX_VALUE)
-                leftO[j] = 0;
-            else
-                leftO[j] = j - leftIdx;
-        }
-    }
-
-    private static void right(char[] input, char hold, int[] rightO) {
-        int rightIdx = Integer.MAX_VALUE;
-        for (int j = input.length - 1; 0 <= j; j--) {
-            if (input[j] == hold)
-                rightIdx = j;
-
-            if (rightIdx == Integer.MAX_VALUE)
-                rightO[j] = 0;
-            else
-                rightO[j] = rightIdx - j;
-        }
     }
 }
