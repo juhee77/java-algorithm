e869e94096b99f0954e98a5bb8e1fe5df10e9ca0
juhee77 <bora2233@naver.com>
2025-05-20 23:55:19 +0900

    basic : 0520

---
+++ src/프로그래머스/level1/Programmers_142086_가장가까운같은글자.java
@@ -0,0 +1,16 @@
+package 프로그래머스.level1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Programmers_142086_가장가까운같은글자 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+
+    }
+}
