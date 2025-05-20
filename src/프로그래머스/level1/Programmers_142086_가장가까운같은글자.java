3cafa230e432f35ec1af7e61a2ac42235888dead
juhee77 <bora2233@naver.com>
2025-05-21 00:03:19 +0900

    basic : 0520

--- src/프로그래머스/level1/Programmers_142086_가장가까운같은글자.java
+++ src/프로그래머스/level1/Programmers_142086_가장가까운같은글자.java
@@ -1,16 +1,23 @@
 package 프로그래머스.level1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Programmers_142086_가장가까운같은글자 {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        int[] charIndex = new int[26];
+        Arrays.fill(charIndex, -1);
+        for (int i = 0; i < s.length(); i++) {
+            int temp = s.charAt(i) - 'a';
+            answer[i] = (charIndex[temp] == -1) ? -1 : i - charIndex[temp];
+            charIndex[temp] = i;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-
+        Programmers_142086_가장가까운같은글자 programmers142086_가장가까운같은글자 = new Programmers_142086_가장가까운같은글자();
+        System.out.println(Arrays.toString(programmers142086_가장가까운같은글자.solution("banana")));
     }
 }
