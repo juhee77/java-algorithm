a823ae86350ed81055dac1dfa54de628b55ac26b
juhee77 <bora2233@naver.com>
2025-06-15 22:47:40 +0900

    dp : 0615

---
+++ src/백준/dynamicProgramming/Boj_4811_알약.java
@@ -0,0 +1,32 @@
+package 백준.dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Boj_4811_알약 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        BigInteger[] catalan = new BigInteger[31];
+        catalan[0] = BigInteger.ONE;
+        int nowHold = 1;
+
+        int input;
+        while ((input = Integer.parseInt(br.readLine())) != 0) {
+            if (nowHold < input) {
+                for (int i = 1; i <= input; i++) {
+                    catalan[i] = BigInteger.ZERO;
+                    for (int j = 0; j < i; j++) {
+                        catalan[i] = catalan[i].add(catalan[j].multiply(catalan[i - 1 - j]));
+                    }
+                }
+                nowHold = input;
+            }
+            sb.append(catalan[input]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
