cee2c4561b07605e3ec972afcf79718f0dc48922
lahezy <jouhi3322@gmail.com>
2025-09-08 23:39:10 +0900

    dp : 0908

---
+++ src/백준/dynamicProgramming/Boj_15989_123더하기4.java
@@ -0,0 +1,37 @@
+package 백준.dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_15989_123더하기4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[1][2] = 0;
+        dp[1][3] = 0;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[2][3] = 0;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(dp[input][1] + dp[input][2] + dp[input][3]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
