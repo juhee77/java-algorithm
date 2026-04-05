157246e9fa41a82bc1786812901fcb7ec2346ab3
juhee77 <bora2233@naver.com>
2026-04-05 22:45:52 +0900

    basic : 0405

---
+++ src/백준/basic/Boj_10599_다면체.java
@@ -0,0 +1,24 @@
+package 백준.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_10599_다면체 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int ans = 2 + E - V;
+            sb.append(ans).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
